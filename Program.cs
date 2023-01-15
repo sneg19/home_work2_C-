9bdfb5c7cc7e28618cea860677691ba3a6830269
sneg <sneg.sbg@gmail.com>
2023-01-15 15:39:42 +0300

    normal_commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// a=5, b=7
+int number_1 = 5;
+int number_2 = 7;
+if (number_1 > number_2)
+{
+    Console.WriteLine($"Наибольшее число {number_1}, а наименьшее {number_2}");
+}
+else
+{
+    Console.WriteLine($"Наибольшее число {number_2}, а наименьшее {number_1}");
+}
+
+// a=2, b=10
+int number_3 = 2;
+int number_4 = 10;
+if (number_3 > number_4)
+{
+    Console.WriteLine($"Наибольшее число {number_3}, а наименьшее {number_4}");
+}
+else
+{
+    Console.WriteLine($"Наибольшее число {number_4}, а наименьшее {number_3}");
+}
+// a=-9, b=-3
+int number_5 = -9;
+int number_6 = -3;
+if (number_1 > number_2)
+{
+    Console.WriteLine($"Наибольшее число {number_5}, а наименьшее {number_6}");
+}
+else
+{
+    Console.WriteLine($"Наибольшее число {number_6}, а наименьшее {number_5}");
+}
